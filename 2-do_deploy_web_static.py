343482af3adfc4907b714eccac7202d3a18e8d3d
Saibantoril <110996980+Saibantoril@users.noreply.github.com>
2023-04-06 14:42:53 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,61 @@
-
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-A Fabric script that distributes an archive to my web servers using the function do_deploy 
+Distributes an archive to web servers using Fabric and the do_deploy function.
 """
-from fabric import Connection
-from datetime import datetime
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+from fabric.api import *
+from os import path
+
+env.hosts = ['<IP web-01>', '<IP web-02>']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 def do_deploy(archive_path):
-    try:
-        c = Connection(env.hosts[0])
-        if not c.is_connected():
-            c.connect()
-        if not c.run('test -d /data/web_static/releases/').ok:
-            c.sudo('mkdir -p /data/web_static/releases/')
-        timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-        c.put(archive_path, '/tmp')
-        c.run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/'.format(
-            archive_path.split('/')[-1]))
-        c.run('sudo rm /tmp/{}'.format(archive_path.split('/')[-1]))
-        c.run('sudo mv /data/web_static/releases/web_static /data/web_static/releases/{}'.
-              format(timestamp))
-        c.run('sudo rm -rf /data/web_static/current')
-        c.run('sudo ln -s /data/web_static/releases/{} /data/web_static/current'.
-              format(timestamp))
-        c.close()
-    except Exception as e:
+    """
+    Deploys web files to server
+    """
+    if not path.exists(archive_path):
         return False
+
+    try:
+        # Upload archive
+        put(archive_path, '/tmp/')
+
+        # Create target directory
+        archive_filename = archive_path.split("/")[-1]
+        archive_basename = archive_filename.split(".")[0]
+        release_path = "/data/web_static/releases/" + archive_basename
+        run("sudo mkdir -p " + release_path)
+
+        # Uncompress archive and delete .tgz
+        run("sudo tar -xzf /tmp/" + archive_filename + " -C " + release_path + "/")
+        run("sudo rm /tmp/" + archive_filename)
+
+        # Move contents into host web_static
+        run("sudo mv " + release_path + "/web_static/* " + release_path + "/")
+        run("sudo rm -rf " + release_path + "/web_static")
+
+        # Delete pre-existing symbolic link
+        run("sudo rm -rf /data/web_static/current")
+
+        # Create new symbolic link
+        run("sudo ln -s " + release_path + " /data/web_static/current")
+    except Exception as e:
+        print(e)
+        return False
+
     return True
+
+def deploy():
+    """
+    Runs the do_deploy function on all hosts in env.hosts
+    """
+    archive_path = "web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    local("tar -czvf {} web_static/".format(archive_path))
+
+    results = execute(do_deploy, archive_path)
+
+    if all(results.values()):
+        print("All deployments successful")
+    else:
+        print("Deployment failed on some hosts")
