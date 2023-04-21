6153eaf746bf1dec07b66c706e53c20925608521
Saibantoril <110996980+Saibantoril@users.noreply.github.com>
2023-04-21 22:19:48 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" holds class State"""
+import models
 from models.base_model import BaseModel, Base
-from models.city import City
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class State(BaseModel, Base):
-    """
-    State class
-    """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        cities = relationship("City", cascade="all, delete", backref="state")
+    """Representation of state """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128),
+                      nullable=False)
+        cities = relationship("City", cascade="all, delete",
+                              backref="states")
     else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """
-            Returns the list of City instances with state_id
-            equal to the current State.id
-            """
-            from models import storage
-            city_list = []
-            for city in storage.all(City).values():
+            """fs getter attribute that returns City instances"""
+            values_city = models.storage.all("City").values()
+            list_city = []
+            for city in values_city:
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    list_city.append(city)
+            return list_city
