95dd85b31574f7d7d21ebb407cbe492193e7bd87
root <saheedolabanji@gmail.com>
2023-04-21 03:59:31 -0700

    added HTML for task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -53,13 +53,10 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """
-    Takes an integer from the URL and renders an HTML template with the
-    value of the integer in an H1 tag.
-    """
-    return render_template('number.html', number=n)
-
+def number_template(n=None):
+    """display a HTML page only if n is an integer"""
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
