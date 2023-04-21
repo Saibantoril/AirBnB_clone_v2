c307c8481f69581aefd00ec6c1ef4fcaad471590
root <saheedolabanji@gmail.com>
2023-04-21 09:47:24 -0700

    fixed task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -66,8 +66,6 @@ def number_odd_or_even(n):
             return render_template('6-number_odd_or_even.html', result="even", number=n)
         else:
             return render_template('6-number_odd_or_even.html', result="odd", number=n)
-    else:
-        return "Invalid input!"
-
+   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
