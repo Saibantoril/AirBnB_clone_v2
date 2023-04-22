ce873f7282b7d045d4ca65c0cefe60b6555ad6cc
root <saheedolabanji@gmail.com>
2023-04-22 03:11:44 -0700

    added task 7 and HTML file

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+This module starts a Flask web application to display a list of states from a storage engine.
+"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Renders an HTML page that displays a list of all states sorted by name.
+
+    Returns:
+        A string with the HTML content of the page.
+    """
+    states = storage.all(State).values()
+    states_sorted = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states_sorted)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Closes the SQLAlchemy session after each request.
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
